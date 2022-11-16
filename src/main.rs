782b4dd996851ff86ed64030ed00161e865dd74d
CoderCowMoo <broskichad38@gmail.com>
2022-11-16 16:31:31 +1100

    Read a line of input

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@
 // [0-9]+[ ]*[hms]*(inutes|econds|ours|our|econd|inute|ins|ecs)*
 // This regex matches the following strings:
 // 10m20s (splits up into 10m and 20s)
-// 10minutes is allowed.
+// 10minutes is allowed as well.
 
+use regex::Regex;
 fn main() {
-    println!("Hello, world!");
+    println!("How long should this timer go for? ");
+    // get a line from input
+    let mut input = String::new();
+    match std::io::stdin().read_line(&mut input) {
+        Ok(n) => n,
+        Err(error) => panic!("Error reading input: {}", error),
+    };
 }
