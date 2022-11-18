72d5c60c0b2e64d57f48a859efb995995f09aa50
CoderCowMoo <broskichad38@gmail.com>
2022-11-18 13:59:14 +1100

    Add to time to sleep based on token

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,9 @@ fn main() {
         Err(error) => panic!("Error reading input: {}", error),
     };
 
+    // init number of seconds to sleep for
+    let mut num_seconds = 0;
+
     // create a regex to match the input
     let token_re = match Regex::new("[0-9]+[ ]*[hms]+(inutes|econds|ours|our|econd|inute|ins|ecs)*")
     {
@@ -22,12 +25,38 @@ fn main() {
         Err(error) => panic!("Error creating regex: {}", error),
     };
 
+    // create a regex for each type of token.
     let hours_re = Regex::new("[0-9]+[ ]*[h]+(ours|our)*").unwrap();
     let mins_re = Regex::new("[0-9]+[ ]*[m]+(inutes|inute|ins)*").unwrap();
     let secs_re = Regex::new("[0-9]+[ ]*[s]+(econds|econd|ecs)*").unwrap(); // completely made up by Copilot. Insane
 
+    // split the input into tokens
     for token_match in token_re.find_iter(&input) {
         // check if it refers to hours, minutes or seconds.
         // we'll use another regex to check for each case.
+        let token_text = token_match.as_str();
+        // then we have to get the number from the token
+        // this is an incredibly 'best practices' way dreamt up by Copilot.
+        // I was about to use another regex lol.
+        let time = match token_text
+            .chars()
+            .filter(|c| c.is_numeric())
+            .collect::<String>()
+            .parse::<u32>()
+        {
+            Ok(n) => n,
+            Err(error) => panic!("Error parsing number from token: {}", error),
+        };
+        if hours_re.is_match(token_text) {
+            num_seconds += time * 60 * 60;
+        }
+        if mins_re.is_match(token_text) {
+            num_seconds += time * 60;
+        }
+        if secs_re.is_match(token_text) {
+            num_seconds += time;
+        } else {
+            println!("Invalid token: {}", token_text);
+        }
     }
 }
